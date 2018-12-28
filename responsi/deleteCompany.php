3b8c4dc936517c073e8a7173e5e04ed472c1c5cc
fizikurniawan <fizikurniawan@gmail.com>
2018-12-28 23:36:19 +0700

    crud crud

--- responsi/deleteCompany.php
+++ responsi/deleteCompany.php
@@ -1,7 +1,7 @@
 <?php
 	include "cons.php";
 	$id = $_GET['id'];
-	$query = "DELETE FROM company WHERE idcompany = '".$id."' ";
+	$query = "DELETE FROM company WHERE idcompany = $id ";
 	$sql = mysqli_query($kon,$query);
 	if ($sql) {
 		?>
